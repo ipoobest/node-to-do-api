9c00847e3a9047ffb58c2b12060b939dfa85f885
hoikhong <57010993@kmitl.ac.th>
2018-12-04 23:29:49 +0700

    mongoose

---
+++ server/server.js
@@ -0,0 +1,39 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+const url = 'mongodb://localhost:27017/TodoApp';
+mongoose.connect(url, { useNewUrlParser: true });
+
+var Todo = mongoose.model('Todo', {
+    text: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    },
+    completedAt: {
+        type: Number
+    }
+});
+
+// var newTodo = new Todo({
+//     text: 'Cook dinner'
+// });
+
+// newTodo.save().then((doc) => {
+//     console.log('Save todo', doc);
+// }, (e) => {
+//     console.log('Unale to save todo');
+// })
+
+var otherTodo = new Todo({
+    text: 'Feed the cat',
+    completed: true,
+    completedAt: 123
+});
+
+otherTodo.save().then((doc) => {
+    console.log(JSON.stringify(doc, undefined, 2));
+}, (e) => {
+    console.log('Unale to save todo');
+});
